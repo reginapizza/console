9c91ac32618ba97489748fa09e7787f450f8e87d
Regina Scott <rescott@redhat.com>
2020-11-04 14:45:38 -0500

    added argocd link to gitops dashboard details page

--- frontend/packages/dev-console/src/components/gitops/details/GitOpsDetailsPageHeading.tsx
+++ frontend/packages/dev-console/src/components/gitops/details/GitOpsDetailsPageHeading.tsx
@@ -2,11 +2,9 @@ import * as React from 'react';
 import { BreadCrumbs, ResourceIcon, ExternalLink } from '@console/internal/components/utils';
 import { Split, SplitItem, Label } from '@patternfly/react-core';
 import { routeDecoratorIcon } from '../../import/render-utils';
-// import { ExternalLinkAltIcon, LinkIcon } from '@patternfly/react-icons';
-import { ExternalLinkAltIcon } from '@patternfly/react-icons';
 import './GitOpsDetailsPageHeading.scss';
 
-import * as _ from 'lodash';
+import * as _ from 'lodash-es';
 import { useK8sWatchResource } from '@console/internal/components/utils/k8s-watch-hook';
 import { referenceForModel } from '@console/internal/module/k8s';
 import { ConsoleLinkModel } from '@console/internal/models';
@@ -15,7 +13,6 @@ interface GitOpsDetailsPageHeadingProps {
   url: string;
   appName: string;
   manifestURL: string;
-  // argoURL: string;
   badge?: React.ReactNode;
 }
 
@@ -23,7 +20,6 @@ const GitOpsDetailsPageHeading: React.FC<GitOpsDetailsPageHeadingProps> = ({
   url,
   appName,
   manifestURL,
-  // argoURL,
   badge,
 }) => {
   const breadcrumbs = [
@@ -37,10 +33,6 @@ const GitOpsDetailsPageHeading: React.FC<GitOpsDetailsPageHeadingProps> = ({
     },
   ];
 
-<<<<<<< HEAD
-  const argoURL =
-    'https://argocd-server-argocd.apps.dev-svc-4.6-103007.devcluster.openshift.com/applications';
-=======
   const [consoleLinks] = useK8sWatchResource({
     isList: true,
     kind: referenceForModel(ConsoleLinkModel),
@@ -49,12 +41,13 @@ const GitOpsDetailsPageHeading: React.FC<GitOpsDetailsPageHeadingProps> = ({
   let aLink;
   _.filter(consoleLinks, (link: any) => {
     if (link.spec.location === 'ApplicationMenu') {
-      aLink = link;
-      return true;
+      if (link.metadata.name === "argocd") {
+        aLink = link;
+        return true;
+      }
+      return false;
     }
-    return false;
   });
->>>>>>> f119ea6aed30732b569e4e9da2c75c7fb51c5fef
 
   return (
     <div className="odc-gitops-details-page-heading co-m-nav-title co-m-nav-title--breadcrumbs">
@@ -85,26 +78,15 @@ const GitOpsDetailsPageHeading: React.FC<GitOpsDetailsPageHeadingProps> = ({
           </Label>
         </SplitItem>
       </Split>
-<<<<<<< HEAD
       <Split className="odc-gitops-details-page-heading__argocd" hasGutter>
-        <SplitItem>Argo CD:</SplitItem>
-        <SplitItem isFilled>
-          <a href={argoURL} rel="noopener noreferrer" target="_blank">
-            https://argocd-server-argocd.apps.dev-svc-4.6-103007.devcluster.openshift.com/applications
-            <ExternalLinkAltIcon style={{ fontSize: 12, marginLeft: 5, marginRight: 5 }} />
-          </a>
-        </SplitItem>
-=======
-      <Split className="odc-gitops-details-page-heading__repo" hasGutter>
         {aLink && (
           <>
-            <SplitItem>{aLink.spec.text}:</SplitItem>
-            <dd>
+            <SplitItem style={{ fontWeight: 'bold' }}>{aLink.spec.text}:</SplitItem>
+            <SplitItem isFilled>
               <ExternalLink href={aLink.spec.href} text={aLink.spec.href} />
-            </dd>
+            </SplitItem>
           </>
         )}
->>>>>>> f119ea6aed30732b569e4e9da2c75c7fb51c5fef
       </Split>
     </div>
   );
